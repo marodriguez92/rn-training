1aa34f1d853608032fe3c04974716531eb5ce6b9
martin <ma.rodrig92@gmail.com>
2019-05-30 00:36:24 -0300

    Did exercise 4.

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,40 +1,25 @@
 import React from 'react';
-import { StyleSheet, ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import styles from './styles';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <ScrollView horizontal={false/*true*/}>
-          <View style={styles.box1}><Text>{'Box 1'}</Text></View>
-          <View style={styles.box2}><Text>{'Box 2'}</Text></View>
-          <View style={styles.box3}><Text>{'Box 3'}</Text></View>
+          <View style={styles.box1}>
+            <Text style={styles.textSM}>{'Box 1'}</Text>
+          </View>
+          <View style={styles.box2}>
+            <Text style={styles.textME}>{'Box 2'}</Text>
+          </View>
+          <View style={styles.box3}>
+            <Text style={styles.textBG}>{'Box 3'}
+              <Text style={[styles.textBG, styles.textRed]}>{' with red text'}</Text>
+            </Text>
+          </View>
         </ScrollView>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
-  box1: {
-    height: 200,
-    width: 500,
-    backgroundColor: 'red'
-  },
-  box2: {
-    height: 300,
-    width: 500,
-    backgroundColor: 'yellow'
-  },
-  box3: {
-    height: 400,
-    width: 500,
-    backgroundColor: 'green'
-  }
-});
