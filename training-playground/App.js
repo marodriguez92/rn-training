2a19b11faa4d0f30e5ac3cfc16e9ad22b561bae6
martin <ma.rodrig92@gmail.com>
2019-05-30 00:45:19 -0300

    Did exercise 5.

--- training-playground/App.js
+++ training-playground/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { View, Image } from 'react-native';
 import styles from './styles';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView horizontal={false/*true*/}>
-          <View style={styles.box1}>
-            <Text style={styles.textSM}>{'Box 1'}</Text>
-          </View>
-          <View style={styles.box2}>
-            <Text style={styles.textME}>{'Box 2'}</Text>
-          </View>
-          <View style={styles.box3}>
-            <Text style={styles.textBG}>{'Box 3'}
-              <Text style={[styles.textBG, styles.textRed]}>{' with red text'}</Text>
-            </Text>
-          </View>
-        </ScrollView>
+        <Image
+          style={{ height: 50, width: 50 }}
+          source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+          resizeMode={'contain'}
+        />
+        <Image
+          style={{ height: 50, width: 50 }}
+          source={require('./assets/icon.png')}
+          resizeMode={'contain'}
+        />
       </View>
     );
   }
